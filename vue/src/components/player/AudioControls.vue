128b0813e51acaebaf6bdefe633344a21ada5b06
adem <adem@adems-Laptop.local>
2025-03-20 15:13:01 +1100

    changed name to vue

---
+++ vue/src/components/player/AudioControls.vue
@@ -0,0 +1,47 @@
+<script setup lang="ts">
+import { Button, Slider } from 'primevue';
+import { computed } from 'vue';
+
+const { volume, muted } = defineProps<{volume: number, muted: boolean}>()
+
+const emit = defineEmits<{
+    'update:volume': [value: number]
+    'update:muted': [value: boolean]
+}>()
+
+const volumeIcon = computed(() => {
+    if(muted) {
+        return "pi pi-times"
+    }
+
+    if(volume >= .65) {
+        return "pi pi-volume-up"
+    } else if(volume <= 0.65 && volume > 0) {
+        return "pi pi-volume-down"
+    }
+
+    return "pi pi-volume-off"
+})
+
+const updateVolumeSlider = (event: Event) => {
+    const target = event.target as HTMLInputElement;
+    emit("update:volume", target.valueAsNumber)
+}
+
+const updateMutedButton = () => {
+    emit("update:muted", !muted)
+}
+
+</script>
+
+<template>
+    <div class="flex">
+        <Button class="!aspect-auto !min-w-12 !bg-highlight" aria-label="Volume Controls" :icon="volumeIcon" @click="updateMutedButton"></Button>
+        <input 
+            type="range"
+            class="max-w-24"
+            @input="updateVolumeSlider"
+            :min="0" :max="1" :step="0.1"/>
+
+    </div>
+</template>
---
+++ vue/src/components/schedule/ScheduleItem.vue
@@ -0,0 +1,57 @@
+<script setup lang="ts">
+    import {type EventDisplay, type FormattedScheduleEvent } from '@/lib/generateScheduleData';
+    import type { Artist } from '@/lib/types';
+
+    const { event } = defineProps<{
+        event: FormattedScheduleEvent,
+        artist?: Artist
+    }>()
+
+
+</script>
+
+<template>
+    <div class="mx-auto max-w-[600px] font-sans">
+        <h1 class="bg-white w-fit font-bold">
+            {{ event.start.toFormat("h:mma") }} - {{ event.end.toFormat("h:mma") }}
+        </h1>
+    
+        <div
+            class="border-b-black border-b-2 max-w-[66%]"
+        ></div>
+    
+        <div
+            :style='{backgroundColor: event.color === "white" ? "purple" : event.color}'
+            class="grid grid-cols-[15%_15%_auto]
+            max-w-[72] mx-auto ml-4 text-white"
+        >
+            <div
+                class="[writing-mode:vertical-rl] ml-auto flex items-center justify-center"
+            >
+                <h3 class="">
+                    {{
+                        {
+                            playlist: "PLIST",
+                            mix: "PRE",
+                            live: "LIVE",
+                        }[event.media]
+                    }}
+                </h3>
+            </div>
+            <div
+                class="flex items-center justify-center text-2xl">
+                {{ event.media === "playlist" ? "♫" : "☆" }}
+            </div>
+            <div>
+                <h2
+                    class="font-bold overflow-ellipsis overflow-hidden min-h-12 inline-block break-all"
+                >
+                    <span>
+                        {{ event.title }}
+                    </span>
+                </h2>
+            </div>
+        </div>
+    </div> 
+
+</template>
